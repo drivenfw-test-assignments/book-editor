de1f02cc9f5439c1fe30fb23891979c6b6f14bc5
drivenfw <hey@drivenfw.com>
2018-12-14 19:49:40 +0300

    Header's controls are shown depending on the current path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -42,30 +42,35 @@ const Header = ({
     <Route path="/books/:id/edit" render={() =>
       <PageTitle title="Edit Book" />
     } />
-    <Link className="header__createbook" to="/books/new">
-      <button className="createbook">
-        New Book
-      </button>
-    </Link>
-    <div className="sortby">
-      <div className="sortby__label">
-        Sort by:
+    <Route path="/books/new" children={({ match }) =>
+      !match && 
+        <Link className="header__createbook" to="/books/new">
+          <button className="createbook">
+            New Book
+          </button>
+        </Link>
+    } />
+    <Route exact path="/" render={() =>
+      <div className="sortby">
+        <div className="sortby__label">
+          Sort by:
+        </div>
+        <select
+          className="sortby__field"
+          value={field}
+          onChange={onFieldChange}
+        >
+          <option value="title">Title</option>
+          <option value="yearOfPublication">Year of publication</option>
+        </select>
+        <button 
+          className="sortby__direction"
+          onClick={onDirectionChange}
+        >
+          {direction === 'asc' ? '▲' : '▼'}
+        </button>
       </div>
-      <select
-        className="sortby__field"
-        value={field}
-        onChange={onFieldChange}
-      >
-        <option value="title">Title</option>
-        <option value="yearOfPublication">Year of publication</option>
-      </select>
-      <button 
-        className="sortby__direction"
-        onClick={onDirectionChange}
-      >
-        {direction === 'asc' ? '▲' : '▼'}
-      </button>
-    </div>
+    } />
   </div>
 
 export default withRouter(connect(
