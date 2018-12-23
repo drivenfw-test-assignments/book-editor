3540c2079c4a4212a7d7eb8b6f2352d7e4022905
drivenfw <hey@drivenfw.com>
2018-12-23 21:13:22 +0300

    Refactoring

--- src/components/Book/BookField.js
+++ src/components/Book/BookField.js
@@ -4,10 +4,10 @@ import React from 'react'
 const BookField = ({ content, title }) =>
   content
     ? <div className="book__field">
-        <div className="book__fieldtitle">
+        <div className="book__field-title">
           {title}:
         </div>
-        <div className="book__fieldcontent">
+        <div className="book__field-content">
           {content}
         </div>
       </div>
