19a8ba06da22e5b9b4aefb6b3e292f8bca074001
drivenfw <hey@drivenfw.com>
2018-12-31 22:32:24 +0300

    Props typechecking added to PageTitle component

--- src/components/Header/PageTitle.js
+++ src/components/Header/PageTitle.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 
 const PageTitle = ({ title }) =>
   <div className="header__page-title">
     / {title}
   </div>
+
+PageTitle.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default PageTitle
 
