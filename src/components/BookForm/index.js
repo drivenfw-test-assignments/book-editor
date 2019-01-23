55164249cbf2f3af1e55113b7b456cac991f4e86
drivenfw <hey@drivenfw.com>
2019-01-23 23:28:12 +0300

    Input for loading image added to BookForm component

--- src/components/BookForm/index.js
+++ src/components/BookForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Form, Field } from 'react-final-form'
 import arrayMutators from 'final-form-arrays'
@@ -8,215 +8,247 @@ import './styles.css'
 
 const required = value => (value ? undefined : "Required")
 
-const BookForm = ({ 
-  initialValues,
-  onSubmit
-}) =>
-  <Form
-    initialValues={initialValues}
-    mutators={{
-      ...arrayMutators
-    }}
-    onSubmit={onSubmit}
-    render={({ 
-      handleSubmit, 
-      form: { mutators: { push, pop }, reset },
-      submitting, 
-      pristine, 
-      values 
-    }) => (
-      <form 
-        className="book-form"
-        onSubmit={handleSubmit}
-      >
-        <Field name="title" validate={required}>
-          {({ input, meta }) => (  
-            <div className="form-group">
-              <label className="form-group__label">Title</label>
-              <input 
-                className="form-group__input"
-                {...input} 
-                type="text" 
-                placeholder="Title"
-              />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
-            </div>
-          )}
-        </Field>
-        <FieldArray name="authors">
-          {({ fields }) => 
-            fields.map((name, index) => (
-              <div key={name}>
-                <Field 
-                  name={`${name}.name`}
-                  validate={required}
-                >
-                  {({ input, meta }) => (  
-                    <div className="form-group">
-                      <label className="form-group__label">Author</label>
-                      <input 
-                        className="form-group__input"
-                        {...input} 
-                        type="text" 
-                        placeholder="Name"
-                      />
-                      <span className="form-group__error">
-                        {meta.error && meta.touched && meta.error}
-                      </span>
-                    </div>
-                  )}
-                </Field>
-                <Field
-                  name={`${name}.surname`}
-                  validate={required}
-                >
-                  {({ input, meta }) => (  
-                    <div className="form-group">
-                      <label className="form-group__label">
-                        {index > 0 && 
-                          <span
-                            className="form-group__delete-author"
-                            onClick={() => fields.remove(index)}
-                          >
-                            ❌
+class BookForm extends Component {
+  imageInput = React.createRef()
+
+  onSubmit = values => {
+    const file = this.imageInput.current.files[0]
+
+    if (file) {
+      // TODO: show loader
+      const reader = new FileReader()
+      
+      reader.onload = e => {
+        values.imageUrl = e.target.result
+        this.props.onSubmit(values) 
+      }
+
+      reader.readAsDataURL(file)
+    } else {
+      this.props.onSubmit(values) 
+    }
+  }
+
+  render() {
+    const { initialValues } = this.props
+
+    return (
+      <Form
+        initialValues={initialValues}
+        mutators={{
+          ...arrayMutators
+        }}
+        onSubmit={this.onSubmit}
+        render={({ 
+          handleSubmit, 
+          form: { mutators: { push, pop }, reset },
+          submitting, 
+          pristine, 
+          values 
+        }) => (
+          <form 
+            className="book-form"
+            onSubmit={handleSubmit}
+          >
+            <Field name="title" validate={required}>
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">Title</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="text" 
+                    placeholder="Title"
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
+            <FieldArray name="authors">
+              {({ fields }) => 
+                fields.map((name, index) => (
+                  <div key={name}>
+                    <Field 
+                      name={`${name}.name`}
+                      validate={required}
+                    >
+                      {({ input, meta }) => (  
+                        <div className="form-group">
+                          <label className="form-group__label">Author</label>
+                          <input 
+                            className="form-group__input"
+                            {...input} 
+                            type="text" 
+                            placeholder="Name"
+                          />
+                          <span className="form-group__error">
+                            {meta.error && meta.touched && meta.error}
                           </span>
-                        }
-                      </label>
-                      <input 
-                        className="form-group__input"
-                        {...input} 
-                        type="text" 
-                        placeholder="Surname"
-                      />
-                      <span className="form-group__error">
-                        {meta.error && meta.touched && meta.error}
-                      </span>
-                    </div>
-                  )}
-                </Field>
-              </div>
-            ))
-          }
-        </FieldArray>
-        <div className="form-group">
-          <button 
-            className="book-form__add-author-btn"
-            onClick={e => {
-              e.preventDefault()
-              push('authors', undefined)
-            }}
-          >
-            Add Author
-          </button>
-        </div>
-        <Field name="pages" validate={required}>
-          {({ input, meta }) => (  
+                        </div>
+                      )}
+                    </Field>
+                    <Field
+                      name={`${name}.surname`}
+                      validate={required}
+                    >
+                      {({ input, meta }) => (  
+                        <div className="form-group">
+                          <label className="form-group__label">
+                            {index > 0 && 
+                              <span
+                                className="form-group__delete-author"
+                                onClick={() => fields.remove(index)}
+                              >
+                                ❌
+                              </span>
+                            }
+                          </label>
+                          <input 
+                            className="form-group__input"
+                            {...input} 
+                            type="text" 
+                            placeholder="Surname"
+                          />
+                          <span className="form-group__error">
+                            {meta.error && meta.touched && meta.error}
+                          </span>
+                        </div>
+                      )}
+                    </Field>
+                  </div>
+                ))
+              }
+            </FieldArray>
             <div className="form-group">
-              <label className="form-group__label">Pages</label>
-              <input 
-                className="form-group__input"
-                {...input} 
-                type="number" 
-                placeholder="Pages"
-                min="0"
-                max="10000"
-              />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
+              <button 
+                className="book-form__add-author-btn"
+                onClick={e => {
+                  e.preventDefault()
+                  push('authors', undefined)
+                }}
+              >
+                Add Author
+              </button>
             </div>
-          )}
-        </Field>
-        <Field name="publisher">
-          {({ input, meta }) => (  
+            <Field name="pages" validate={required}>
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">Pages</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="number" 
+                    placeholder="Pages"
+                    min="0"
+                    max="10000"
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
+            <Field name="publisher">
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">Publisher</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="text" 
+                    placeholder="Publisher"
+                    min="0"
+                    max="10000"
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
+            <Field name="yearOfPublication">
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">Year of publication</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="number" 
+                    placeholder="Year of publication"
+                    min="1800"
+                    max={(new Date()).getFullYear()}
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
+            <Field name="releaseDate">
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">Release date</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="date" 
+                    placeholder="Release date"
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
+            <Field name="isbn">
+              {({ input, meta }) => (  
+                <div className="form-group">
+                  <label className="form-group__label">ISBN</label>
+                  <input 
+                    className="form-group__input"
+                    {...input} 
+                    type="text" 
+                    placeholder="ISBN"
+                  />
+                  <span className="form-group__error">
+                    {meta.error && meta.touched && meta.error}
+                  </span>
+                </div>
+              )}
+            </Field>
             <div className="form-group">
-              <label className="form-group__label">Publisher</label>
-              <input 
+              <label className="form-group__label">Image</label>
+              <input
+                ref={this.imageInput}
                 className="form-group__input"
-                {...input} 
-                type="text" 
-                placeholder="Publisher"
-                min="0"
-                max="10000"
+                type="file" 
               />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
             </div>
-          )}
-        </Field>
-        <Field name="yearOfPublication">
-          {({ input, meta }) => (  
-            <div className="form-group">
-              <label className="form-group__label">Year of publication</label>
-              <input 
-                className="form-group__input"
-                {...input} 
-                type="number" 
-                placeholder="Year of publication"
-                min="1800"
-                max={(new Date()).getFullYear()}
-              />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
+            <div className="form-buttons">
+              <button 
+                className="form-buttons__btn"
+                type="submit" 
+                disabled={submitting}
+              >
+                Submit
+              </button>
+              <button
+                className="form-buttons__btn"
+                type="button"
+                onClick={reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
             </div>
-          )}
-        </Field>
-        <Field name="releaseDate">
-          {({ input, meta }) => (  
-            <div className="form-group">
-              <label className="form-group__label">Release date</label>
-              <input 
-                className="form-group__input"
-                {...input} 
-                type="date" 
-                placeholder="Release date"
-              />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
-            </div>
-          )}
-        </Field>
-        <Field name="isbn">
-          {({ input, meta }) => (  
-            <div className="form-group">
-              <label className="form-group__label">ISBN</label>
-              <input 
-                className="form-group__input"
-                {...input} 
-                type="text" 
-                placeholder="ISBN"
-              />
-              <span className="form-group__error">
-                {meta.error && meta.touched && meta.error}
-              </span>
-            </div>
-          )}
-        </Field>
-        <div className="form-buttons">
-          <button 
-            className="form-buttons__btn"
-            type="submit" 
-            disabled={submitting}
-          >
-            Submit
-          </button>
-          <button
-            className="form-buttons__btn"
-            type="button"
-            onClick={reset}
-            disabled={submitting || pristine}
-          >
-            Reset
-          </button>
-        </div>
-      </form>
-    )}
-  />
+          </form>
+        )}
+      />
+    )
+  }
+}
 
 BookForm.propTypes = {
   initialValues: PropTypes.object.isRequired,
