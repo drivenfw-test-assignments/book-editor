320aab56dfd7358c30385dce695c68134ef44d52
drivenfw <hey@drivenfw.com>
2019-03-15 23:12:18 +0300

    Message component renders a route

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,25 +3,27 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import Transition from 'react-transition-group/transition'
+import { Route, withRouter } from 'react-router'
 import './styles.css'
 
 
 const Message = ({ message }) =>
-  <Transition
-    in={!!message}
-    timeout={200}
-    unmountOnExit
-  >
-    {state => (
-      <div className={classNames('message', state)}>
-        {message}
-      </div>
-    )}
-  </Transition>
+  <Route render={() =>
+    <Transition
+      in={!!message}
+      timeout={200}
+    >
+      {state => (
+        <div className={classNames('message', state)}>
+          {message}
+        </div>
+      )}
+    </Transition>
+  } />
 
 Message.propTypes = {
   message: PropTypes.string
 }
 
-export default connect(({ message }) => ({ message }))(Message)
+export default connect(({ message }) => ({ message }))(withRouter(Message))
 
