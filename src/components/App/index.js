1f17bc7114bdbff7156292d59744e8e0481647a3
drivenfw <hey@drivenfw.com>
2019-05-18 13:51:54 +0300

    Refactoring

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,7 +10,7 @@ import Message from 'components/Message'
 import EditBook from 'pages/EditBook'
 import BookList from 'pages/BookList'
 import NewBook from 'pages/NewBook'
-import { NEW_BOOK_PATH, ROOT_PATH } from 'def'
+import { EDIT_BOOK_PATH, NEW_BOOK_PATH, ROOT_PATH } from 'def'
 import './styles.css'
 
 
@@ -22,7 +22,7 @@ const App = () =>
       <Switch>
         <Route exact path={ROOT_PATH} component={BookList} />
         <Route path={NEW_BOOK_PATH} component={NewBook} />
-        <Route path="/books/:id/edit" component={EditBook} />
+        <Route path={EDIT_BOOK_PATH} component={EditBook} />
         <Route render={() => <Redirect to={ROOT_PATH} />} />
       </Switch>
     </div>
