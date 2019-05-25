04de03f2ea96f697d812a5e07daaf4a488d62a16
drivenfw <hey@drivenfw.com>
2019-05-25 23:45:03 +0300

    FileInput renders input

--- src/components/BookForm/FileInput.js
+++ src/components/BookForm/FileInput.js
@@ -2,8 +2,34 @@ import React, { Component } from 'react'
 
 
 class FileInput extends Component {
+  imageInput = React.createRef()
+
+  onImageChange = e => {
+    const file = this.imageInput.current.files[0]
+
+    if (file) {
+      const reader = new FileReader()
+      
+      reader.onload = e => {
+        const imageUrl = e.target.result
+
+        this.props.onFileInputChange(imageUrl)
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
   render () {
-    return <div className="file-input" />
+    return (
+      <div className="file-input">
+        <input
+          ref={this.imageInput}
+          type="file" 
+          onChange={this.onImageChange}
+        />
+      </div>
+    )
   }
 }
 
