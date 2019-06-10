5667cd5d3fa21421946cebda27da404ac44b65f6
drivenfw <hey@drivenfw.com>
2019-06-10 21:06:56 +0300

    Styles added to BackButton component

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 
 const BackButton = () =>  
-  <div className="back-button" />
+  <div className="back-button">
+    ⬅
+  </div>
 
 export default BackButton
 
