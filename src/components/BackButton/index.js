db23440e06f657f91f580602be1021fd57b07aca
drivenfw <hey@drivenfw.com>
2019-06-11 22:52:50 +0300

    BackButton updated to render a link

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ROOT_PATH } from 'def'
 import './styles.css'
 
 
 const BackButton = () =>  
   <div className="back-button">
-    ⬅
+    <Link to={ROOT_PATH}>
+      ⬅
+    </Link>
   </div>
 
 export default BackButton
